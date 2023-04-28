8c1ab751b7e3ab13bfa527034e9747af72ff3d94
sagar-ss <sagarsrivastava0302@gmail.com>
2023-04-28 14:00:16 +0530

    cess to fetch at 'http://localhost:4000//api/workouts' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useEffect, useState} from 'react';
+
+const Home = () => {
+  const[workouts,setWorkouts] = useState(null);
+  useEffect(()=>{
+
+    const fetchWorkout = async()=>{
+      const response = await fetch('http://localhost:4000//api/workouts')
+      const json = await response.json(); // for coverting it into array of objects
+
+      if(response.ok)
+      {
+        setWorkouts(json);
+      }
+    }
+    fetchWorkout();
+
+  },[])
+  return (
+    <div className='home'>
+        <div className='workouts'>
+          {workouts && workouts.map((workout)=>(
+            <p key={workout._id}>{workout.title}</p>
+          ))}
+        </div>
+    </div>
+  )
+}
+
+export default Home
