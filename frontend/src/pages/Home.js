71bac4af55f89b1622c688e1f78046f2fbe8d898
sagar-ss <sagarsrivastava0302@gmail.com>
2023-04-28 22:15:17 +0530

    useContext and useReducer hook for syncing the database and state

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useEffect, useState} from 'react';
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
   const[workouts,setWorkouts] = useState(null);
@@ -21,9 +23,10 @@ const Home = () => {
     <div className='home'>
         <div className='workouts'>
           {workouts && workouts.map((workout)=>(
-            <p key={workout._id}>{workout.title}</p>
+            <WorkoutDetails key={workout._id} workout={workout} />
           ))}
         </div>
+        <WorkoutForm/>
     </div>
   )
 }
